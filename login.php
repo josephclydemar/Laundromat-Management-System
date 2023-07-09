76db2aded7b91f9c597aa3d21244bc81a511db93
Kyle Angela Mar <markyleangela@gmail.com>
2023-07-10 01:25:27 +0800

    add user authentication feature. use 'database_connection.php' for modular database connection objects.

--- login.php
+++ login.php
@@ -1,11 +1,51 @@
 <?php
     session_start();
-    // echo 'username: '.$_POST['username'].'<br>';
-    // echo 'password: '.$_POST['password'].'<br>';
-    if(isset($_POST)) { 
-        $_SESSION['username'] = $_POST['username'];
-        $_SESSION['password'] = $_POST['password'];
-        header('Location: index.php');
-        return;
+    include_once "database_connection.php";
+
+    function authenticateUser($entered_email, $entered_password)
+    {
+        $db_connection = new DatabaseConnection();
+        $pdo = $db_connection->getPDO();
+        $sql_select = "SELECT * FROM customers WHERE email=:email";
+        $stmt = $pdo->prepare($sql_select);
+        $stmt->execute(array(':email'=>$entered_email));
+	    $select_query_result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($select_query_result)
+        {
+            $recorded_password = $select_query_result['customer_password'];
+            if($recorded_password == $entered_password)
+            {
+                // Account exists, correct password.
+                return $select_query_result['customer_id'];
+            }
+            else
+            {
+                // Incorrect password.
+                return 0;
+            }
+        }
+        else
+        {
+            // Account does not exist.
+            return 0;
+        }
+    }
+
+    if(isset($_POST)) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $id_authenticated_user = authenticateUser($email, $password);
+        if($id_authenticated_user != 0) {
+            if(isset($_SESSION['customer_id'])) {
+                $_SESSION['customer_id'] = $id_authenticated_user;
+            }
+            header('Location: customer_track_orders.php');
+            return;
+        }
+        else
+        {
+            header('Location: index.php');
+            return;
+        }
     }
 ?>
