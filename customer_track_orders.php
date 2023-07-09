3e6c9f3ade8a6738083f599f5a724f4cd313892a
Kyle Angela Mar <markyleangela@gmail.com>
2023-07-09 23:58:08 +0800

    correct spelling mistake.

--- customer_track_orders.php
+++ customer_track_orders.php
@@ -9,8 +9,8 @@
         $sql_select = "SELECT * FROM customers WHERE customer_id=:customer_id";
 	    $stmt = $pdo->prepare($sql_select);
         $stmt->execute(array(':customer_id'=>$_SESSION['customer_id']));
-	    $resto_query_result = $stmt->fetch(PDO::FETCH_ASSOC);
-        foreach($resto_query_result as $key => $value) {
+	    $select_query_result = $stmt->fetch(PDO::FETCH_ASSOC);
+        foreach($select_query_result as $key => $value) {
             echo $key.' : '.$value.'<br>';
         }
     }
